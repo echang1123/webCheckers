23f35b054b1c962bcb0145b86f5153ddbe0b5031
Eugene <exc8593@rit.edu>
2018-03-02 03:41:24 -0500

    just added a line for gethomeroute

---
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -0,0 +1,36 @@
+package com.webcheckers.ui;
+
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+/**
+ * Created by Eugene on 3/1/2018.
+ */
+public class PostSignInRoute implements Route{
+
+    private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
+    private final TemplateEngine templateEngine;
+    private HashMap<String, Object> players = new HashMap<>();
+
+    public PostSignInRoute(TemplateEngine templateEngine, final HashMap<String,Object> players) {
+        // validation
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        Objects.requireNonNull(templateEngine, "players must not be null");
+        //
+        this.templateEngine = templateEngine;
+        this.players = players;
+        //
+        LOG.config("PostSignInRoute is initialized.");
+    }
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        final Session session = request.session();
+
+        //session.at;
+//        final PlayerLobby playerLobby = session.attribute("signedin", true);
+        return null;
+    }
+}
