80dd4bc9064ce64e08ab51a8e01913c6bacfcfe4
Eugene <exc8593@rit.edu>
2018-03-02 12:28:50 -0500

    not sure for lobby class

--- src/main/java/com/webcheckers/ui/PlayerLobby.java
+++ src/main/java/com/webcheckers/ui/PlayerLobby.java
@@ -1,6 +1,8 @@
 package com.webcheckers.ui;
 
+import spark.TemplateEngine;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
@@ -8,9 +10,16 @@ import java.util.logging.Logger;
  */
 public class PlayerLobby {
     private static final Logger LOG = Logger.getLogger(PlayerLobby.class.getName());
+    private final TemplateEngine templateEngine;
+    private HashMap<String, Object> players = new HashMap<>();
+    private Boolean signedIn;
 
+    public PlayerLobby (final TemplateEngine templateEngine, final HashMap<String,Object> players) {
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        this.templateEngine = templateEngine;
+        this.players = players;
+        this.signedIn = false;
 
-    public PlayerLobby () {
         LOG.fine("New player lobby instance created.");
     }
 }
