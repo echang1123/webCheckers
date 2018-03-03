8fa9f5f3487bcf05e256f440b2f6e67219cc194c
Karthik Iyer <kni6228@rit.edu>
2018-03-03 16:51:08 -0500

    Minor refactoring

--- src/main/java/com/webcheckers/ui/GetSignOutRoute.java
+++ src/main/java/com/webcheckers/ui/GetSignOutRoute.java
@@ -1,6 +1,14 @@
+/**
+ * GET "/signout" route handler
+ *
+ * @author Eugene Chang
+ * @author Karthik Iyer
+ * @author Hongda Lin
+ */
+
+
 package com.webcheckers.ui;
 
-import com.webcheckers.model.Player;
 import spark.*;
 
 import java.util.HashMap;
@@ -8,59 +16,54 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
-/**
- * Created by Eugene on 3/2/2018.
- */
-public class GetSignOutRoute implements Route{
-    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
 
+public class GetSignOutRoute implements Route{
+
+    private static final Logger LOG = Logger.getLogger( GetSignInRoute.class.getName() );
+
+    // Keys
     static final String PLAYER_LOBBY_KEY = "playerLobby";
     static final String SIGNED_IN = "isSignedIn";
     static final String CURRENT_PLAYER = "currentPlayer";
     static final String PLAYERS = "players";
 
+    // Attributes
     private final TemplateEngine templateEngine;
-    private HashMap<String, Object> players;
+    private HashMap< String, Object > players;
+
 
     /**
-     * Create the Spark Route (UI controller) for the
-     * {@code GET /} HTTP request.
-     *
-     * @param templateEngine
-     *   the HTML template rendering engine
+     * Constructor for the GetSignOutRoute route handler
+     * @param templateEngine  the HTML template rendering engine
      */
-    public GetSignOutRoute(final TemplateEngine templateEngine, final HashMap<String,Object> players) {
+    public GetSignOutRoute( final TemplateEngine templateEngine, final HashMap< String,Object > players ) {
         // validation
-        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        Objects.requireNonNull( templateEngine, "templateEngine must not be null" );
         //
         this.templateEngine = templateEngine;
         this.players = players;
         //
-        LOG.config("GetSignOutRoute is initialized.");
+        LOG.config( "GetSignOutRoute is initialized." );
     }
+
 
     /**
      * Render the signed-in WebCheckers Home page.
-     *
-     * @param request
-     *   the HTTP request
-     * @param response
-     *   the HTTP response
-     *
-     * @return
-     *   the rendered HTML for the Home page
+     * @param request the HTTP request
+     * @param response  the HTTP response
+     * @return the rendered HTML for the Home page
      */
     @Override
-    public Object handle(Request request, Response response) {
-        LOG.finer("GetSignOutRoute is invoked.");
+    public Object handle( Request request, Response response ) {
+        LOG.finer( "GetSignOutRoute is invoked." );
         //
-        Map<String, Object> vm = new HashMap<>();
-        vm.put("title", "Welcome!");
+        Map< String, Object > vm = new HashMap<>();
+        vm.put( "title", "Welcome!" );
         final Session session = request.session();
-        String username = session.attribute(CURRENT_PLAYER);
-        if(players.containsKey(username))
+        String username = session.attribute( CURRENT_PLAYER );
+        if( players.containsKey( username ) )
             players.remove(username);
-        session.attribute(SIGNED_IN,false);
+        session.attribute( SIGNED_IN,false );
         response.redirect( WebServer.HOME_URL );
         return null;
     }
