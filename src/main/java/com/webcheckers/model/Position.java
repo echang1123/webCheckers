b47e2277de6413d6d9d5396beb39bdd4a90bba1d
Karthik Iyer <kni6228@rit.edu>
2018-03-15 09:12:47 -0400

    Implemented Position class

---
+++ src/main/java/com/webcheckers/model/Position.java
@@ -0,0 +1,46 @@
+/*
+ * Class that represents a position on the checkers board
+ * Contains the row index and the space ( cell ) index
+ *
+ * @author Karthik Iyer
+ */
+
+
+package com.webcheckers.model;
+
+
+public class Position {
+
+	private int row; // the index of the row
+	private int cell; // the index of the space ( cell )
+
+
+	/**
+	 * Constructor for the Position class
+	 * @param row the index of the row
+	 * @param cell the index of the space ( cell )
+	 */
+	public Position( int row, int cell ) {
+		this.row = row;
+		this.cell = cell;
+	}
+
+
+	/**
+	 * Getter for the row index
+	 * @return the row index of the position
+	 */
+	public int getRow() {
+		return this.row;
+	}
+
+
+	/**
+	 * Getter for the space ( cell ) index
+	 * @return the space ( cell ) index of the position
+	 */
+	public int getCell() {
+		return this.cell;
+	}
+
+}
