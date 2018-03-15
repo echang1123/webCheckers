7f7b1e722fc045852f93f8f441fdde031bb84564
Karthik Iyer <kni6228@rit.edu>
2018-03-15 13:26:01 -0400

    Message is Serializable, stubbed out PostValidateMoveRoute

---
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -0,0 +1,29 @@
+/*
+ * POST "/validateMove" handler
+ *
+ * @author Karthik Iyer
+ */
+
+
+package com.webcheckers.ui;
+
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+
+public class PostValidateMoveRoute implements Route {
+
+
+	/**
+	 * Handles the POST "/validateMove" request
+	 * @param request the request
+	 * @param response the response
+	 * @return the Object containing the return data
+	 */
+	@Override
+	public Object handle( Request request, Response response ) {
+		return null;
+	}
+}
