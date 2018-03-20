dc2c3fc8def924d376a325a1f7cd29fb1fb983be
Karthik Iyer <kni6228@rit.edu>
2018-03-20 18:12:52 -0400

    Unit tests complete for Piece and PostSignInRoute

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -9,6 +9,7 @@ package com.webcheckers.model;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/com/webcheckers/ui/PostSignInRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostSignInRouteTest.java
@@ -16,6 +16,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -26,20 +27,20 @@ import spark.*;
 public class PostSignInRouteTest {
 
 	private static final String VALID_USERNAME = "Karthik";
-	private static final String QUOTES_USERNAME = "\"Karthik";
+	private static final String QUOTES_USERNAME = "\"Karthik\"";
 	private static final String EMPTY_USERNAME = "";
 
 	// Component under test
 	private PostSignInRoute CuT;
 
-	// Friendly objects - (none)
+	// Friendly objects
+	private PlayerLobby playerLobby; // we need this to be real
 
 	// Mock objects
 	private Session session;
 	private Request request;
 	private Response response;
 	private TemplateEngine templateEngine;
-	private PlayerLobby playerLobby;
 
 
 	/**
@@ -52,7 +53,7 @@ public class PostSignInRouteTest {
 		session = mock(Session.class);
 		when(request.session()).thenReturn(session);
 		templateEngine = mock(TemplateEngine.class);
-		playerLobby = mock(PlayerLobby.class);
+		playerLobby = new PlayerLobby();
 		CuT = new PostSignInRoute(templateEngine, playerLobby);
 	}
 
@@ -98,5 +99,46 @@ public class PostSignInRouteTest {
 		templateEngineTester.assertViewModelAttribute("message","Username cannot contain quotes");
 		// assert view name
 		templateEngineTester.assertViewName("signin.ftl");
+	}
+
+
+	/**
+	 * Tests when a sign-in with a valid username is tried
+	 */
+	@Test
+	public void valid_username() {
+		// setup
+		when(request.queryParams("name")).thenReturn(VALID_USERNAME);
+		final TemplateEngineTester templateEngineTester = new TemplateEngineTester();
+		when(templateEngine.render(any(ModelAndView.class))).thenAnswer(templateEngineTester.makeAnswer());
+		// invoke the test
+		CuT.handle(request, response);
+		// check view model
+		templateEngineTester.assertViewModelNotExists();
+	}
+
+
+	/**
+	 * Tests sign-in attempt with a username that is taken
+	 */
+	@Test
+	public void same_username() {
+		// setup
+		when(request.queryParams("name")).thenReturn(VALID_USERNAME);
+		final TemplateEngineTester templateEngineTester = new TemplateEngineTester();
+		when(templateEngine.render(any(ModelAndView.class))).thenAnswer(templateEngineTester.makeAnswer());
+		playerLobby.addPlayer(new Player(VALID_USERNAME), null); // add a valid username to the lobby
+		// invoke the test
+		CuT.handle(request,response);
+		// check view model
+		templateEngineTester.assertViewModelExists();
+		templateEngineTester.assertViewModelIsaMap();
+		// check data in view model
+		templateEngineTester.assertViewModelAttribute("title", "Sign In");
+		templateEngineTester.assertViewModelAttribute("message",
+				"The username Karthik is already taken");
+		// assert view name
+		templateEngineTester.assertViewName("signin.ftl");
+
 	}
 }
