69eb0d0db512419d669a5707306436093d0bf4a8
Eugene <exc8593@rit.edu>
2018-03-20 20:39:42 -0400

    tests for space and gethomeroute completed

---
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -0,0 +1,30 @@
+package com.webcheckers.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Eugene on 3/15/2018.
+ */
+@Tag("Model-Tier")
+public class SpaceTest {
+    private Space s = new Space(0,null,false);
+
+    @Test
+    public void test_spaceExists(){
+        assertNotNull(s, "The Space exists");
+    }
+
+    @Test
+    public void test_setPiece(){
+        s.setPiece(new Piece(Piece.PieceType.SINGLE, Piece.Color.RED));
+        assertNotNull(s.getPiece(), "The Piece Exists after setPiece()");
+    }
+
+    @Test
+    public void test_isValid(){
+        assertFalse(s.isValid(),"Is Valid works correctly");
+    }
+
+}
