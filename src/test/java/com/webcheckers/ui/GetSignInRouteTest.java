72ec3f95b0f451e8e1bdf65507cfdcebc6cb13cc
Emily <eaw7163@rit.edu>
2018-03-25 15:43:33 -0400

    route unit test in progress

---
+++ src/test/java/com/webcheckers/ui/GetSignInRouteTest.java
@@ -0,0 +1,18 @@
+/*
+ * This class contains unit tests for the GetSignInRoute class
+ *
+ * @author Emily Wesson
+ */
+
+package com.webcheckers.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("ui-tier")
+public class GetSignInRouteTest {
+
+
+}
