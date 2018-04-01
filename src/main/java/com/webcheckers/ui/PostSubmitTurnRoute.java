58f734dc338f731b48072a5731f6e7360d923752
Emily <eaw7163@rit.edu>
2018-03-31 22:21:35 -0400

    "updated submitTurnRoute for case if you took opponent's last piece and you won- sends you back home with victory message"

--- src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
@@ -2,6 +2,7 @@
  * POST "/submitTurn" handler
  *
  * @author Karthik Iyer
+ * @Emily Wesson
  */
 
 
@@ -13,12 +14,11 @@ import com.webcheckers.appl.GlobalInformation;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.appl.RoutesAndKeys;
 import com.webcheckers.model.*;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import spark.Session;
+import spark.*;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
@@ -28,17 +28,20 @@ public class PostSubmitTurnRoute implements Route {
     // Attributes
     private static final Logger LOG = Logger.getLogger( PostSubmitTurnRoute.class.getName() );
     private GlobalInformation gi;
+    private final TemplateEngine templateEngine;
 
 
     /**
      * Constructor for the PostSubmitTurnRoute Handler
      * @param gi the global information
      */
-    public PostSubmitTurnRoute( GlobalInformation gi ) {
+    public PostSubmitTurnRoute( TemplateEngine templateEngine, GlobalInformation gi ) {
         // validate
         Objects.requireNonNull( LOG, "Log cannot be null" );
         Objects.requireNonNull( gi, "gi cannot be null" );
+        Objects.requireNonNull( templateEngine, "templateEngine must not be null" );
 
+        this.templateEngine = templateEngine;
         this.gi = gi;
     }
 
@@ -87,13 +90,44 @@ public class PostSubmitTurnRoute implements Route {
             return new Message( "", Message.MessageType.ERROR );
         }
 
+
+        //get all of the players, remove the current player from that Map
+        HashMap< String, Player > players = playerLobby.getPlayers();
+        Map< String, Player > otherPlayers = new HashMap<>( players );
+        otherPlayers.remove( currentPlayerName ); // remove the current player from being shown
+
+        Map< String, Object > vm = new HashMap<>();
+
         //if you are RED and your opponent is out of pieces, you won
         if( currentPlayer.equals(game.getPlayerOne()) && board.getWhitePiecesInPlay() <= 0){
+
+            currentPlayer.removeOpponent();
+         //   gameLobby.removeGame( game ); when they find out they lost, the opponent will do this in checkTurnRoute
+            httpSession.attribute( RoutesAndKeys.IN_GAME_KEY, false );
+            Message message = new Message( "You are victorious!", Message.MessageType.INFO );
+
+            vm.put( RoutesAndKeys.MESSAGE_KEY, message );
+            vm.put( RoutesAndKeys.CURRENT_PLAYER_KEY, currentPlayerName);
+            vm.put( RoutesAndKeys.PLAYERS_KEY, otherPlayers );
+            vm.put( RoutesAndKeys.SIGNED_IN_KEY, true );
+            game.switchTurn();
+            return templateEngine.render( new ModelAndView( vm, "home.ftl" ) );
 
         }
 
         //if you are WHITE and your opponent is out of pieces, you won
         if( currentPlayer.equals( game.getPlayerTwo() ) && board.getRedPiecesInPlay() <= 0 ){
+
+            currentPlayer.removeOpponent();
+            httpSession.attribute( RoutesAndKeys.IN_GAME_KEY, false );
+            Message message = new Message( "You are victorious!", Message.MessageType.INFO );
+
+            vm.put( RoutesAndKeys.MESSAGE_KEY, message );
+            vm.put( RoutesAndKeys.CURRENT_PLAYER_KEY, currentPlayerName);
+            vm.put( RoutesAndKeys.PLAYERS_KEY, otherPlayers );
+            vm.put( RoutesAndKeys.SIGNED_IN_KEY, true );
+            game.switchTurn();
+            return templateEngine.render( new ModelAndView( vm, "home.ftl" ) );
 
         }
 
