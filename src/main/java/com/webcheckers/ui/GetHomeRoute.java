ccf627c610f10c329eb04be453421d138830953c
Eugene <exc8593@rit.edu>
2018-04-15 19:44:41 -0400

    working now!

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -88,7 +88,7 @@ public class GetHomeRoute implements Route {
             // check if you have been selected for a game
             Game game = gameLobby.findGame( currentPlayer );
 
-            if( game != null ) {
+            if( game != null && !game.isComplete() ) {
                 response.redirect( RoutesAndKeys.GAME_URL );
             }
 
